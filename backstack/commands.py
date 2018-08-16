cf21cecf685ba2b5337b206aa1d27a0a398415fe
Sumit Datta <sumitdatta@gmail.com>
2018-08-16 10:35:39 +0530

    Minor changes

--- backstack/commands.py
+++ backstack/commands.py
@@ -62,12 +62,37 @@ class Commands(object):
         )
         channel.queue_declare(exclusive=True)
 
+    def create_schema_ddl(self):
+        import inspect
+        from sqlalchemy import create_engine
+        from sqlalchemy.schema import CreateTable
+        from sqlalchemy.dialects import postgresql
+
+        for app in settings.APPS:
+            # try:
+            #     models = importlib.import_module("apps.%s.models" % app)
+            #     for x in inspect.getmembers(models, inspect.isclass):
+            #         cls = getattr(models, x[0])
+            #         if issubclass(cls, Base) and hasattr(cls, "__tablename__"):
+            #             print(CreateTable(cls.__table__).compile(dialect=postgresql.dialect()))
+            # except ModuleNotFoundError:
+            #     pass
+            self.__app.load_models()
+
+            def metadata_dump(sql, *multiparams, **params):
+                # print or write to log or file etc
+                print(sql.compile(dialect=engine.dialect))
+
+            engine = create_engine(settings.DB_DEFAULT, convert_unicode=True, strategy="mock", executor=metadata_dump)
+            Base.metadata.create_all(engine)
+
     def execute(self):
         parser = argparse.ArgumentParser(description="default backend")
         parser.add_argument(
             "action",
             action="store",
-            choices=["server", "drop_tables", "create_tables", "load_fixtures", "run_workers", "shell"]
+            choices=["server", "drop_tables", "create_tables", "load_fixtures", "run_workers", "create_schema_ddl",
+                     "shell"]
         )
 
         args = parser.parse_args()
@@ -83,6 +108,8 @@ class Commands(object):
             self.load_fixtures()
         elif args.action == "run_workers":
             self.workers()
+        elif args.action == "create_schema_ddl":
+            self.create_schema_ddl()
         elif args.action == "shell":
             import code
             code.interact(local=locals())
