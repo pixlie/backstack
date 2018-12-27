49adde6bbb22810f315f807f79988f549296015b
Sumit Datta <sumitdatta@gmail.com>
2018-12-27 19:03:49 +0530

    S3 Endpoint URL for DigitalOcean support

--- backstack/config.py
+++ backstack/config.py
@@ -59,6 +59,7 @@ class Settings(metaclass=Singleton):
 
         self.FILE_UPLOAD_PATH = config("FILE_UPLOAD_PATH", cast=str, default="/tmp/")
         self.S3_BUCKET = config("S3_BUCKET", cast=str)
+        self.S3_ENDPOINT_URL = config("S3_ENDPOINT_URL", cast=str)
 
         self.SESSION_COOKIE_NAME = config("SESSION_COOKIE_NAME", cast=str)
 
